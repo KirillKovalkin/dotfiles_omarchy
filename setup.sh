510ec3b039d2113dccbb876e5e69fcb3dc9e9727
Kirill Kovalkin <kiryll.kovalkin@gmail.com>
2025-10-13 05:28:47 +0300

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+[[ -d "hypr" && -d "waybar" ]] || {
+    echo "❌ Missing hypr or waybar directories."
+    exit 1
+}
+
+echo "🧹 Deleting old configurations"
+rm -rf "$HOME/.config/hypr/" "$HOME/.config/waybar"
+
+echo "📦 Copying new configurations"
+cp -r hypr "$HOME/.config/"
+cp -r waybar "$HOME/.config/"
+echo "✅ New configurations is installed"
+
+echo "📥 Installing packages"
+sudo pacman -S tree telegram-desktop firefox discord 7zip --noconfirm
+yay -S polychromatic google-chrome yandex-music-native --noconfirm
+sudo gpasswd -a $USER plugdev
+echo "✅ Packages is installed"
+
